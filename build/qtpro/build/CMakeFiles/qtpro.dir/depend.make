37749202eb12e87ccd1443b4232111ba432e245d
Tytus <dbednorz@tuta.io>
2025-04-02 10:11:17 +0200

    main.cpp updated. Added function to create a Qt5 project

---
+++ build/qtpro/build/CMakeFiles/qtpro.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for qtpro.
+# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <string>
 
+//TODO: Debug the program, when creating the project with option "-qt" at the end it shows
+// error messeage saying that creating a directory failed but it creates anyways. For some 
+// reason it also creates the files with the standard content instead of those meant for Qt5
+//
+// Last update: 01.04.2025
 
 int main(int argc, char* argv[]){
 	std::string projectName;
@@ -22,6 +28,38 @@ int main(int argc, char* argv[]){
 		"\n"
 		"add_executable(main main.cpp)";
 
+	const std::string qtCmakeContent = 
+		"cmake_minimum_required(VERSION 3.10)\n"
+		"project(" + projectName + " VERSION 1.0 LANGUAGES CXX)\n"
+		"\n"
+		"set(CMAKE_CXX_STANDARD 17)\n"
+		"set(CMAKE_CXX_STANDARD_REQUIRED ON)\n"
+		"\n"
+		"find_package(Qt5 REQUIRED COMPONENTS Widgets)\n"
+		"\n"
+		"add_executable(" + projectName + " main.cpp)\n"
+		"\n"
+		"target_link_libraries(" + projectName + " PRIVATE Qt5::Widgets)";
+
+	const std::string qtMainContent = 
+		"#include <QApplication>\n"
+		"#include <QMainWindow>\n"
+		"#include <QString>\n"
+		"\n"
+		"QString projectName = \"" + projectName + "\";"
+		"\n"
+		"int main(int argc, char *argv[]){\n"
+		"QApplication app(argc, argv);\n"
+		"\n"
+		"QMainWindow mainWindow;\n"
+		"mainWindow.setWindowTitle(projectName);\n"
+		"mainWindow.resize(800, 600);\n"
+		"mainWindow.show();\n"
+		"\n"
+		"return app.exec();\n"
+		"\n"
+		"}";
+
 	const std::string mainContent = 
 		"#include <iostream>\n"
 		"\n"
@@ -36,7 +74,7 @@ int main(int argc, char* argv[]){
 		std::cout << "Usage: cpproject <projectname>" << std::endl;
 	}
 
-	if(argc > 2){
+	if(argc > 3){
 		std::cout << "Too much arguments!" << std::endl;
 		std::cout << "Usage: cpproject <projectname>" << std::endl;
 		return 1;
@@ -45,6 +83,11 @@ int main(int argc, char* argv[]){
 
 	// CREATING PROJECT FILES AND FOLDER
 	std::filesystem::create_directory(projectName);
+	if(!std::filesystem::create_directory(projectName)){
+		std::cout << "Something went wrong while creating the directory. Please check if you have required priviliges and if the folder already exists" << std::endl;
+	}
+
+
 	std::ofstream cmakeFile(projectName + "/CMakeLists.txt");
 	std::ofstream mainFile(projectName + "/main.cpp");
 
@@ -60,16 +103,21 @@ int main(int argc, char* argv[]){
 	}
 	
 	//WRITING CHANGES TO ALREADY CREATED FILES 
-	cmakeFile << cmakeContent; 
-	cmakeFile.close();
+	
+	if(argc > 2 && std::string(argv[2]) == "-qt"){
+		cmakeFile << qtCmakeContent;
+		cmakeFile.close();
 
-	mainFile << mainContent;
-	mainFile.close();
+		mainFile << qtMainContent;
+		mainFile.close();
+	}
+	else{
+		cmakeFile << cmakeContent;
+		cmakeFile.close();
 
-
+		mainFile << mainContent;
+		mainFile.close();
+	}
+	
 }
-//ERROR CODES:
-//0 - NO_ERROR
-//1 - TOO_MUCH_ARGS
-//5 - CMAKE_FILE_FAILED_TO_CREATE
-//6 - CPP_FILE_FAILED_TO_CREATE
+
